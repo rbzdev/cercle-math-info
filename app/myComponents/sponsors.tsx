da6b154717f2797956406c0bd3c331ab3565b314
rbzdev <rubuzolivier2@gmail.com>
2025-03-15 14:16:48 +0100

    Disable images optimization (Vercel free optimization qota is attempt)

--- app/myComponents/sponsors.tsx
+++ app/myComponents/sponsors.tsx
@@ -10,23 +10,25 @@ export default function Sponsors() {
   return (
     <section className="w-full overflow:hidden flex flex-col gap-4">
       <div className="flex items-center justify-between w-full">
-        <h2 className="text-lg md:text-3xl"> Nos sponsores </h2>
+        <h2 className="text-xl md:text-3xl"> Nos sponsores </h2>
 
         <BecomeSponsor />
       </div>
       <div>
         <Marquee>
           {sponsors.map((sponsor, index) => (
-            <div key={index} className="mr-6 md:mr-12">
-              <div className="flex flex-col items-center gap-2">
+            <div key={index} className="mr-2 md:mr-12">
+              <div className="flex flex-col items-center justify-center gap-2 max-w-36">
                 <Image
                   src={sponsor.imageUrl}
                   alt={sponsor.name}
                   width={100}
                   height={100}
-                  className="rounded-xl bg-white shadow-sm border h-16 w-16 md:h-24 md:w-24 object-contain hover:scale-110 transition-all duration-300"
+                  className="rounded-xl bg-white shadow-sm border h-24 w-24 md:w-40 md:h-36 object-contain hover:scale-110 transition-all duration-300"
                 />
-                <span className="text-xs md:text-sm">{sponsor.name}</span>
+                <span className="text-xs md:text-sm w-full text-center line-clamp-2">
+                  {sponsor.name}
+                </span>
               </div>
             </div>
           ))}
